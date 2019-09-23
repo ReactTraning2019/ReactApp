34b8ae8ecdea9294bfaa272876a2d23cd8b6ffd1
KL14728 <latadkavita@johndeere.com>
2019-09-23 19:16:04 +0530

    feat: added code to connect to API

--- src/myComponent.js
+++ src/myComponent.js
@@ -31,37 +31,60 @@ export default class Component extends React.Component{
     this.setState({show:true});
   }
 
-  render() {
-    if(this.state.show){
-        return (
-          <div>
-          <h2>Students Details</h2>
-          <h3>{`Total Number of students: ${this.state.students.length}`}</h3>
-          <table>
-            <thead>
-              <tr>
-                  <th>Roll number</th>
-                  <th>Name</th>
-              </tr>
-            </thead>
-            {this.state.students.map((student, index)=> (
-              <tbody key={index}>
-                <tr>
-                  <td>{student.rollNo}</td>
-                  <td>{student.name}</td>
-                </tr>
-              </tbody>
-            ))}
-          </table>
-          </div>
-        );
+    componentDidMount() {
+        this.callApi()
+            .then(res => {
+                this.setState({
+                    students: res.map((student) => {
+                        return {
+                            name: student.studentName,
+                            rollNo: student.rollNo
+                        };
+                    })
+                })
+            })
+            .catch(err => console.log(err));
     }
 
-    return(
-        <div>
-            <button onClick={this.showDetails} type='button'>Show student details</button>
-        </div>
-    )
-  }
+    callApi = async () => {
+        const response = await fetch('/getScore');
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+
+        return body;
+    };
+
+    render() {
+        if (this.state.show) {
+            return (
+                <div>
+                    <h2>Students Details</h2>
+                    <h3>{`Total Number of students: ${this.state.students.length}`}</h3>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Roll number</th>
+                            <th>Name</th>
+                        </tr>
+                        </thead>
+                        {this.state.students.map((student, index)=> (
+                            <tbody key={index}>
+                            <tr>
+                                <td>{student.rollNo}</td>
+                                <td>{student.name}</td>
+                            </tr>
+                            </tbody>
+                        ))}
+                    </table>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <button onClick={this.showDetails} type='button'>Show student details</button>
+            </div>
+        )
+    }
 
 }
